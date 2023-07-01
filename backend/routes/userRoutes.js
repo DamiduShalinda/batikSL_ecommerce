6bc7bd7dbee7fe807384d541539e9fae950d94df
TharukaWikum <tharuka99wikum@mail.com>
2023-07-01 21:40:26 +0530

    user Routes, sing in SignUp

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,118 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import expressAsyncHandler from 'express-async-handler';
+
+import User from '../models/userModel.js';
+
+
+const userRouter = express.Router();
+
+
+
+
+userRouter.put(
+  '/profile',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  })
+);
+
+userRouter.post(
+  '/forget-password',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      
+
+      //reset link
+      console.log(`${baseUrl()}/reset-password/${user}`);
+
+      mailgun()
+        .messages()
+        .send(
+          {
+            from: 'Amazona <me@mg.yourdomain.com>',
+            to: `${user.name} <${user.email}>`,
+            subject: `Reset Password`,
+            html: ` 
+             <p>Please Click the following link to reset your password:</p> 
+             <a href="${baseUrl()}/reset-password/${token}"}>Reset Password</a>
+             `,
+          },
+          (error, body) => {
+            console.log(error);
+            console.log(body);
+          }
+        );
+      res.send({ message: 'We sent reset password link to your email.' });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  })
+);
+
+
+
+
+
+
+userRouter.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+userRouter.post(
+  '/signup',
+  expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      
+    });
+  })
+);
+
+export default userRouter;
