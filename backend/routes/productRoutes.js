6fb9570b747b154d2e7499f93e28564318d4cfbf
TharukaWikum <tharuka99wikum@mail.com>
2023-07-05 22:49:10 +0530

    add catogiries to product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -118,4 +118,29 @@ productRouter.get(
   })
 );
 
+productRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
+  })
+);
+
+productRouter.get('/slug/:slug', async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+productRouter.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
 export default productRouter;
