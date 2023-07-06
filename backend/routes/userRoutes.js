95d380194f81e1b8425dba0e270c8d1300987e61
TharukaWikum <tharuka99wikum@mail.com>
2023-07-06 20:03:18 +0530

    add access controll to userRouts

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler';
-
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
-
+import { isAuth, isAdmin, generateToken, baseUrl, mailgun } from '../utils.js';
 
 const userRouter = express.Router();
 
-
-
-
+userRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
+  })
+);
 
 userRouter.get(
-
+  '/:id',
+  isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (user) {
@@ -25,6 +33,7 @@ userRouter.get(
 
 userRouter.put(
   '/profile',
+  isAuth,
   expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
     if (user) {
@@ -54,10 +63,14 @@ userRouter.post(
     const user = await User.findOne({ email: req.body.email });
 
     if (user) {
-      
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '3h',
+      });
+      user.resetToken = token;
+      await user.save();
 
       //reset link
-      console.log(`${baseUrl()}/reset-password/${user}`);
+      console.log(`${baseUrl()}/reset-password/${token}`);
 
       mailgun()
         .messages()
@@ -83,9 +96,52 @@ userRouter.post(
   })
 );
 
+userRouter.post(
+  '/reset-password',
+  expressAsyncHandler(async (req, res) => {
+    jwt.verify(req.body.token, process.env.JWT_SECRET, async (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Invalid Token' });
+      } else {
+        const user = await User.findOne({ resetToken: req.body.token });
+        if (user) {
+          if (req.body.password) {
+            user.password = bcrypt.hashSync(req.body.password, 8);
+            await user.save();
+            res.send({
+              message: 'Password reseted successfully',
+            });
+          }
+        } else {
+          res.status(404).send({ message: 'User not found' });
+        }
+      }
+    });
+  })
+);
+
+userRouter.put(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.isAdmin = Boolean(req.body.isAdmin);
+      const updatedUser = await user.save();
+      res.send({ message: 'User Updated', user: updatedUser });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
 
 userRouter.delete(
-
+  '/:id',
+  isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (user) {
@@ -100,21 +156,18 @@ userRouter.delete(
     }
   })
 );
-
-
-
 userRouter.post(
   '/signin',
   expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
-      if (compareSync(req.body.password, user.password)) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
         res.send({
           _id: user._id,
           name: user.name,
           email: user.email,
           isAdmin: user.isAdmin,
-          
+          token: generateToken(user),
         });
         return;
       }
@@ -137,7 +190,7 @@ userRouter.post(
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      
+      token: generateToken(user),
     });
   })
 );
