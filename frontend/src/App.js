3b974207c4a26b54cc37eac9a39211652e1aa845
Hansini Abeygunwardhana <hchathupomoda@gmail.com>
2023-07-14 10:33:17 +0530

    Homescreen completed and added routes to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import HomeScreen from "./screens/HomeScreen";
+import Container from "react-bootstrap/Container";
+import DashboardScreen from "./screens/DashboardScreen";
+import AdminRoute from "./components/AdminRoute";
 
 function App() {
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div
+        className={
+          sidebarIsOpen
+            ? fullBox
+              ? "site-container active-cont d-flex flex-column full-box"
+              : "site-container active-cont d-flex flex-column"
+            : fullBox
+            ? "site-container d-flex flex-column full-box"
+            : "site-container d-flex flex-column"
+        }
+      >
+        <ToastContainer position="bottom-center" limit={1} />
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route
+                path="/admin/dashboard"
+                element={
+                  <AdminRoute>
+                    <DashboardScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import react from "react";
+import { Helmet } from "react-helmet-async";
+
+function HomeScreen() {
+  return (
+    <div>
+      <Helmet>
+        <title>Batik SL</title>
+      </Helmet>
+      <h1>Featured Products</h1>
+    </div>
+  );
+}
+export default HomeScreen;
