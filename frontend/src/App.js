0a5ad8c93acce1264d8c92e13f91d56a64ac70f9
Dilhara <101543090+Dilhara-99@users.noreply.github.com>
2023-07-14 14:20:52 +0530

    Add search screen to the App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import "./App.css";
+import SearchScreen from "./screens/SearchScreen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Container className="mt-3">
+          <Routes>
+            <Route path="/search" element={<SearchScreen />} />
+          </Routes>
+        </Container>
+      </main>
     </div>
   );
 }
