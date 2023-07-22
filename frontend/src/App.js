cf111c9ebe68362c8822290e5e594057a4042e71
Dilhara <101543090+Dilhara-99@users.noreply.github.com>
2023-07-22 10:20:52 +0530

    add ProductScreen inside App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
                 </AdminRoute>
               }
             ></Route>
+            <Route path="/product/:slug" element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
